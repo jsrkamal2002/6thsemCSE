d8f8a87cfba27113f87c82b41ca90750f1a678be
rajkamal <veerammarish89@gmail.com>
2023-03-14 08:04:59 +0530

    CAT-1

--- CAT-1/Compiler Design/RAW/con1.py
+++ CAT-1/Compiler Design/RAW/con1.py
@@ -1,4 +1,5 @@
 # import module
+import os
 from pdf2image import convert_from_path
 
 
@@ -6,13 +7,19 @@ from pdf2image import convert_from_path
 print("Enter the file name:")
 filename = input()
 print("File name is: " + filename)
-print("started!!!")
+print("Folder Created !!!")
+print("started to convert!!!")
 images = convert_from_path(filename+'.pdf')
-# print(images)
+
+# Folder creation
+if not os.path.isdir(filename):
+    os.mkdir(filename)
+
 
 for i in range(len(images)):
 
     # Save pages as images in the pdf
-    images[i].save('page' + str(i) + '.jpg', 'JPEG')
+    # images[i].save('page' + str(i) + '.jpg', 'JPEG')
+    images[i].save(filename + '/' + 'page' + str(i) + '.jpg', 'JPEG')
 
 print("Done")
